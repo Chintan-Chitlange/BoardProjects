c5c08695f34f0f0c9ae08c9d955a984fe24111a3
Chintan Chitlange <chichichitlange@gmail.com>
2020-05-28 04:07:27 +0100

    updates

--- Question13.java
+++ Question13.java
@@ -1,29 +1,34 @@
 import java.util.Scanner;
 class Question13{
-	public static int binarySearch(int arr[], int first, int last, int key){
-		if (last>=first){
-			int mid = first + (last - first)/2;
-			if (arr[mid] == key)
+	public static int binarySearch(int arr[], int first, int last, int key)
+	{
+	    if (last>=first)
+	    {
+		int mid = first + (last - first)/2;
+		if (arr[mid] == key)
 		{
-			return mid;
+		     return mid;
                 }
-			if (arr[mid] > key){
+                if (arr[mid] > key)
+                {
 			return binarySearch(arr, first, mid-1, key);//search in left subarray
-                        }else{
+                }
+                else
+                {
 			return binarySearch(arr, mid+1, last, key);//search in right subarray
                 }
-		 }
-		return -1;
+            }
+            return -1;
 	}
 	public static void main(String args[]){
-                Scanner sc=new Scanner(System.in);
-		int arr[] = {1982,1987,1993,1996,1999,2003,2006,2007,2009,2010};
-		int key = sc.nextInt();
-                int last=arr.length-1;
-		int result = binarySearch(arr,0,last,key);
-		if (result == -1)
-			System.out.println("Element is not found!");
-		else
-			System.out.println("Element is found at index: "+result);
+            Scanner sc=new Scanner(System.in);
+	    int arr[] = {1982,1987,1993,1996,1999,2003,2006,2007,2009,2010};
+	    int key = sc.nextInt();
+            int last=arr.length-1;
+	    int result = binarySearch(arr,0,last,key);
+	    if (result == -1)
+	        System.out.println("Element is not found!");
+	    else
+		System.out.println("Element is found at index: "+result);
 	}
 }
