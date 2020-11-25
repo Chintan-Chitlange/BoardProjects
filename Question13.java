730ecc86d8c8b6efd289d513b22edd25b1f4d197
Chintan-Chitlange <63422973+Chintan-Chitlange@users.noreply.github.com>
2020-11-25 16:41:20 +0530

    Update Question13.java

--- Question13.java
+++ Question13.java
@@ -1,34 +1,30 @@
 import java.util.Scanner;
-class Question13{
-	public static int binarySearch(int arr[], int first, int last, int key)
-	{
-	    if (last>=first)
-	    {
-		int mid = first + (last - first)/2;
-		if (arr[mid] == key)
-		{
-		     return mid;
-                }
-                if (arr[mid] > key)
-                {
-			return binarySearch(arr, first, mid-1, key);//search in left subarray
-                }
-                else
-                {
-			return binarySearch(arr, mid+1, last, key);//search in right subarray
-                }
-            }
-            return -1;
-	}
-	public static void main(String args[]){
-            Scanner sc=new Scanner(System.in);
-	    int arr[] = {1982,1987,1993,1996,1999,2003,2006,2007,2009,2010};
-	    int key = sc.nextInt();
-            int last=arr.length-1;
-	    int result = binarySearch(arr,0,last,key);
-	    if (result == -1)
-	        System.out.println("Element is not found!");
-	    else
-		System.out.println("Element is found at index: "+result);
-	}
+import java.util.Arrays;
+public class Question14
+{
+    int arr[]=new int[5];
+    void init()
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the temprature of 5 cities");
+        for (int i=0;i<5;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+    }
+    void part2()
+    {
+        Arrays.sort(arr);
+        System.out.println(" ");
+        for(int i=0;i<5;i++)
+        {
+            System.out.print(arr[i]+"\n");
+        }
+    }
+    public static void main()
+    {
+        Question14 obj=new Question14();
+        obj.init();
+        obj.part2();
+    }
 }
